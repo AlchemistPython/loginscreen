ac965c2808b4478b1312db256bbc2a8b810e0b61
David Isaac Alvarez Garcia <alchemistcodepython@gmail.com>
2021-02-13 13:18:51 -0600

    background-color

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.config import Config
+Config.set("graphics","width",400)
+Config.set("graphics","height",200)
 
 class LoginScreen(Widget):
     pass
+
+class Generate(Widget):
+    def press(self):
+        print("Hola mundo")
 
 class DocumentCreator(App):
     def build(self):
